a6e396ab1db8bcf8162afb3320c0e6c569326dc4
chocho13 <darosf@hotmail.fr>
2022-02-06 21:31:17 +0100

    Update and rename token.sol to token-flat.sol

--- token-flat.sol
+++ token-flat.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.0;
 
 /*
  * @dev Provides information about the current execution context, including the
@@ -600,42 +600,55 @@ abstract contract Ownable is Context {
 //base Plop on ERC20Capped + Ownable
 contract Galeon is ERC20Capped, Ownable {
     // big int to store transfer limit
-    uint256 limit;
+    uint limit;
     // bool that represent if limit is enabled
     bool limitEnabled;
+    // address where the initial tokens will be minted and who owns the contract
+    address public immutable multiSig;
 
     // Long name + shortname  and max supply 4 000 000 000 GALEON
-    constructor() ERC20("Galeon", "GALEON") ERC20Capped(4000000000 * 10 ** decimals())  {
+    constructor() ERC20("Galeon", "GALEON") ERC20Capped(4 * 1e9 * 1e18)  {
+        multiSig = 0xc0B3974C64ba269A0Ebd5DD698aC496Fcb817aa9;
         // initial circulating supply 1 056 000 000 GALEON
-        _mint(0xc0B3974C64ba269A0Ebd5DD698aC496Fcb817aa9, 1056000000 * 10 ** decimals());
+        _mint(multiSig, 1.056 * 1e9 * 1e18);
+        // emit Mint event
+        emit Mint(multiSig, 1.056 * 1e9 * 1e18);
         // limit to 85 700 GALEON
-        limit = 85700 * 10 ** decimals();
+        limit = 85700 * 1e18;
         // enable transfer limit
         limitEnabled = true;
+        // emit TransferLimitUnlocked event
+        emit TransferLimitUnlocked(false);
     }
     //function to unlock transfer limit only executable by Owner of the contract
-    function unlockTransferLimit() public onlyOwner {
+    function unlockTransferLimit() external onlyOwner {
         // check if limit is enabled before go further
-        require(limitEnabled == true, "Action can be done only once");
+        require(limitEnabled, "Action can be done only once");
         // assign MAX_INT to limit
         limit = 2**256 - 1;
         // disable bool
         limitEnabled = false;
+        // emit TransferLimitUnlocked event
+        emit TransferLimitUnlocked(true);
     }
     // call function, return if limit is enabled
-    function isTransferLimitEnabled() public view returns (bool) {
+    function isTransferLimitEnabled() external view returns (bool) {
         return limitEnabled;
     }
     // mint function only executable by Owner of the contract
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint amount) external onlyOwner {
         _mint(to, amount);
+        // emit Mint event
+        emit Mint(to, amount);
     }
     // burn function only executable by Owner of the contract
-    function burn(address from, uint256 amount) public onlyOwner {
+    function burn(address from, uint amount) external onlyOwner {
         _burn(from, amount);
+        // emit Burn event
+        emit Burn(from, amount);
     }
     // override default transfer function 
-    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+    function transfer(address recipient, uint amount) public virtual override returns (bool) {
         // if limiteEnabled check if balance + amount is lower than the limit
         if(limitEnabled) {
             require(balanceOf(recipient) + amount <= limit,"85700 GALEON limit Exceeded");
@@ -644,6 +657,10 @@ contract Galeon is ERC20Capped, Ownable {
         super.transfer(recipient, amount);
         return true;
     }
+
+    event TransferLimitUnlocked(bool limitUnlocked);
+    event Mint(address to, uint amount);
+    event Burn(address from, uint amount);
 }
 
 
