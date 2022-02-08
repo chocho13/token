814002f99d2d8bcfc4af4757c6fb1e0067a374d0
chocho13 <darosf@hotmail.fr>
2022-02-08 15:06:35 +0100

    Update staking.sol

--- staking.sol
+++ staking.sol
@@ -3,29 +3,30 @@ pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract OneYearStakingContract is Ownable {
-
-    IERC20 internal constant stakingToken = IERC20(stakingTokenAddress);
+contract OneYearStakingContract is Ownable, ReentrancyGuard {
 
     struct Stake {
         address user;
         uint amount;
         uint sinceBlock;
-        uint lastUpdate;
         uint untilBlock;
+        uint lastUpdate;
         uint unclaimed;
     }
 
-    Stake[] stakes;
+    Stake[] private stakes;
     
     uint public totalSupply;
-    uint public constant maxSupply = 80 * 1e6 * 1e18;
-    uint public constant minimumAmount = 500 * 1e18;
-    uint public constant rewardPerBlock = 0.6808 * 1e18;
     bool public stakingEnabled;
 
-    address public constant stakingTokenAddress = 0x1d0Ac23F03870f768ca005c84cBb6FB82aa884fD; // galeon address
+    uint public constant MAX_SUPPLY = 80 * 1e6 * 1e18;
+    uint public constant MINIMUM_AMOUNT = 500 * 1e18;
+    uint public constant REWARD_PER_BLOCK = 0.6808 * 1e18;
+
+    address public constant STAKING_TOKEN_ADDRESS = 0x1d0Ac23F03870f768ca005c84cBb6FB82aa884fD; // galeon address
+    IERC20 internal constant STAKING_TOKEN = IERC20(STAKING_TOKEN_ADDRESS);
     
     constructor() {
         totalSupply = 0;
@@ -36,24 +37,24 @@ contract OneYearStakingContract is Ownable {
         return stakingEnabled = _enable;
     }
 
-    function stake(uint _amount) external updatePool returns (bool staked) {
+    function stake(uint _amount) external updatePool nonReentrant returns (bool staked) {
         require(stakingEnabled, "Staking is not enabled");
-        require(_amount >= minimumAmount, "Insuficient amount");
-        require(totalSupply + _amount <= maxSupply, "Pool capacity exceeded");
-        require(_amount < stakingToken.balanceOf(msg.sender), "Insuficient balance");
-        require(stakingToken.transferFrom(msg.sender, address(this), _amount), "TransferFrom failed");
+        require(_amount >= MINIMUM_AMOUNT, "Insuficient amount");
+        require(totalSupply + _amount <= MAX_SUPPLY, "Pool capacity exceeded");
+        require(_amount < STAKING_TOKEN.balanceOf(msg.sender), "Insuficient balance");
+        require(STAKING_TOKEN.transferFrom(msg.sender, address(this), _amount), "TransferFrom failed");
         stakes.push(Stake(msg.sender, _amount, block.timestamp, block.timestamp, block.timestamp + 365 days,0));
         totalSupply += _amount;
         return true;
     }
 
-    function claim() external updatePool returns(uint) {
+    function claim() external updatePool nonReentrant returns(uint) {
         uint claimed = 0;
         for(uint i = 0; i < stakes.length; i++) {
             if (stakes[i].user == msg.sender) {
                 if (stakes[i].unclaimed > 0) {
-                    require(stakingToken.balanceOf(address(this)) > stakes[i].unclaimed, "Insuficient contract balance");
-                    require(stakingToken.transfer(msg.sender,stakes[i].unclaimed), "Transfer failed");
+                    require(STAKING_TOKEN.balanceOf(address(this)) > stakes[i].unclaimed, "Insuficient contract balance");
+                    require(STAKING_TOKEN.transfer(msg.sender,stakes[i].unclaimed), "Transfer failed");
                 }
                 claimed += stakes[i].unclaimed;
                 stakes[i].unclaimed = 0;
@@ -62,17 +63,7 @@ contract OneYearStakingContract is Ownable {
         return claimed;
     }
 
-    modifier updatePool() {
-        for(uint i = 0; i < stakes.length; i++) {
-            if(stakes[i].untilBlock >= block.timestamp) {
-                stakes[i].unclaimed += stakes[i].amount / totalSupply * (block.timestamp - stakes[i].lastUpdate) * rewardPerBlock;
-                stakes[i].lastUpdate = block.timestamp;
-            }
-        }
-        _;
-    }
-
-    function unstake() external updatePool {
+    function unstake() external updatePool nonReentrant {
         uint[] memory unstakeIds;
         uint j = 0;
         uint amount;
@@ -83,8 +74,8 @@ contract OneYearStakingContract is Ownable {
                 amount += stakes[i].amount;
             }
         }
-        require(stakingToken.balanceOf(address(this)) > amount, "Insuficient contract balance");
-        require(stakingToken.transfer(msg.sender,amount), "Transfer failed");
+        require(STAKING_TOKEN.balanceOf(address(this)) > amount, "Insuficient contract balance");
+        require(STAKING_TOKEN.transfer(msg.sender,amount), "Transfer failed");
         for(uint i = 0; i < unstakeIds.length; i++) {
             stakes[unstakeIds[i]] = stakes[stakes.length -1];
             stakes.pop();
@@ -102,5 +93,15 @@ contract OneYearStakingContract is Ownable {
             }
         }
         return currents;
+    }
+
+    modifier updatePool() {
+        for(uint i = 0; i < stakes.length; i++) {
+            if(stakes[i].untilBlock >= block.timestamp) {
+                stakes[i].unclaimed += stakes[i].amount / totalSupply * (block.timestamp - stakes[i].lastUpdate) * REWARD_PER_BLOCK;
+                stakes[i].lastUpdate = block.timestamp;
+            }
+        }
+        _;
     }
 }
