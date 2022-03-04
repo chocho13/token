d5d17679be56d6a93db779b113c498b96b5d9a77
chocho13 <darosf@hotmail.fr>
2022-03-04 09:13:02 +0100

    Update LPStaking.sol

--- LPStaking.sol
+++ LPStaking.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.11;
+pragma solidity 0.8.12;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract OneYearStakingContract is Ownable, ReentrancyGuard {
+contract LPStakingContract is Ownable, ReentrancyGuard {
 
     struct Stake {
         address user;
